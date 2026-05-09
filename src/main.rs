99c20dc51fb318b2fcea73795aba3fdbaf8f2e4a
akagiyuu <khanghuynh0245@gmail.com>
2026-05-09 14:58:29 +0700

    feat: add mock repository

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,5 @@
 pub mod model;
+pub mod repository;
 
 fn main() {
     println!("Hello, world!");
---
+++ src/repository/problem.rs
@@ -0,0 +1,16 @@
+use byte_unit::Byte;
+use chrono::Duration;
+use uuid::Uuid;
+
+use crate::model::{Language, Problem};
+
+#[allow(unused)]
+pub fn get(id: Uuid) -> color_eyre::Result<Option<Problem>> {
+    Ok(Some(Problem {
+        checker_language: Language::Cpp,
+        checker_path: "checker".to_string(),
+        time_limit: Duration::seconds(1),
+        memory_limit: Byte::GIBIBYTE,
+        test_cases: vec![],
+    }))
+}
