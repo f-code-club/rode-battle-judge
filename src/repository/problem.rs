7858621bab57f428cf8fc2e4cd04fe24c1399b71
akagiyuu <khanghuynh0245@gmail.com>
2026-05-11 08:01:34 +0700

    fix(repository): enforce entry existence

--- src/repository/problem.rs
+++ src/repository/problem.rs
@@ -5,12 +5,12 @@ use uuid::Uuid;
 use crate::model::{Language, Problem};
 
 #[allow(unused)]
-pub fn get(id: Uuid) -> color_eyre::Result<Option<Problem>> {
-    Ok(Some(Problem {
+pub fn get(id: Uuid) -> color_eyre::Result<Problem> {
+    Ok(Problem {
         checker_language: Language::Cpp,
         checker_path: "checker".to_string(),
         time_limit: Duration::seconds(1),
         memory_limit: Byte::GIBIBYTE,
         test_cases: vec![],
-    }))
+    })
 }
