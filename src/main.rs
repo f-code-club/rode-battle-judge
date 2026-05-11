6e46256e87034ad79f42eed7aed3a002ddc8caaa
akagiyuu <khanghuynh0245@gmail.com>
2026-05-11 08:34:03 +0700

    feat: call queue connect on main

--- src/main.rs
+++ src/main.rs
@@ -2,12 +2,17 @@ pub mod model;
 pub mod queue;
 pub mod repository;
 
-fn main() {
+#[tokio::main]
+async fn main() -> color_eyre::Result<()> {
     tracing_subscriber::fmt()
         .with_max_level(tracing::Level::DEBUG)
         .pretty()
         .with_timer(tracing_subscriber::fmt::time::ChronoLocal::rfc_3339())
         .init();
 
+    queue::connect().await?;
+
     println!("Hello, world!");
+
+    Ok(())
 }
