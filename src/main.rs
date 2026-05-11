d0b844945f7822c56ee7aadab0454fe8c1227857
akagiyuu <khanghuynh0245@gmail.com>
2026-05-11 08:26:49 +0700

    feat: init tracing

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,13 @@
 pub mod model;
-pub mod repository;
 pub mod queue;
+pub mod repository;
 
 fn main() {
+    tracing_subscriber::fmt()
+        .with_max_level(tracing::Level::DEBUG)
+        .pretty()
+        .with_timer(tracing_subscriber::fmt::time::ChronoLocal::rfc_3339())
+        .init();
+
     println!("Hello, world!");
 }
