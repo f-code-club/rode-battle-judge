40f1aa55d96705ad1cbf99cf5f67e407dba78cf6
akagiyuu <khanghuynh0245@gmail.com>
2026-08-24 13:00:42 +0700

    refactor: move to judge sub folder

--- src/model/language.rs
+++
@@ -1,8 +0,0 @@
-#[derive(Debug)]
-pub enum Language {
-    Rust,
-    Cpp,
-    Python,
-    Java,
-    Html,
-}
--- src/model/test_case.rs
+++
@@ -1,5 +0,0 @@
-#[derive(Debug)]
-pub struct TestCase {
-    pub input_path: String,
-    pub output_path: String,
-}
