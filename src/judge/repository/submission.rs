341267f33d24ada63a049f7413ab5807382ee155
akagiyuu <khanghuynh0245@gmail.com>
2026-08-24 15:05:52 +0700

    feat(repository): add update submission

--- src/judge/repository/submission.rs
+++ src/judge/repository/submission.rs
@@ -1,7 +1,7 @@
 use sqlx::PgExecutor;
 use uuid::Uuid;
 
-use super::model::Submission;
+use super::model::{Submission, Verdict};
 
 pub async fn get(executor: impl PgExecutor<'_>, id: Uuid) -> sqlx::Result<Option<Submission>> {
     sqlx::query_as!(
@@ -15,4 +15,29 @@ pub async fn get(executor: impl PgExecutor<'_>, id: Uuid) -> sqlx::Result<Option
     )
     .fetch_optional(executor)
     .await
+}
+
+pub async fn update(
+    executor: impl PgExecutor<'_>,
+    id: Uuid,
+    verdict: Option<Verdict>,
+    score: Option<f32>,
+) -> sqlx::Result<()> {
+    sqlx::query!(
+        r#"
+            UPDATE submissions
+            SET
+                verdict = $2,
+                score = $3,
+                updated_at = now()
+            WHERE id = $1
+        "#,
+        id,
+        verdict as Option<Verdict>,
+        score,
+    )
+    .execute(executor)
+    .await?;
+
+    Ok(())
 }
